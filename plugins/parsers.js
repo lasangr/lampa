5ed1686a821abc676ebf6be998ae4523ceb88cf2
lasangr <162016629+lasangr@users.noreply.github.com>
2024-06-30 18:41:26 +0300

    Update parsers.js

--- plugins/parsers.js
+++ plugins/parsers.js
@@ -21,7 +21,7 @@ Lampa.SettingsApi.addParam({
      description: 'Нажмите для выбора парсера из списка'
     },
     onChange: function (value) {
-     if (value == 'jackett_ru') Lampa.Storage.set('jackett_url', 'https://jct.lasangre.ru')&Lampa.Storage.set('jackett_key', 'kabi795abpkhgfis2h9gz6wxfrhf3bs2', '')&Lampa.Storage.set('jackett_interview','all')&Lampa.Storage.set('parse_in_search', false)&Lampa.Storage.set('parse_lang', 'df');
+     if (value == 'jackett_ru') Lampa.Storage.set('jackett_url', 'https://jct.lasangre.ru/jackett')&Lampa.Storage.set('jackett_key', 'kabi795abpkhgfis2h9gz6wxfrhf3bs2', '')&Lampa.Storage.set('jackett_interview','all')&Lampa.Storage.set('parse_in_search', false)&Lampa.Storage.set('parse_lang', 'df');
      if (value == 'jackett_en') Lampa.Storage.set('jackett_url', 'https://local.lasangre.ru')&Lampa.Storage.set('jackett_key', '558uj0ceztmnbilfe0hjn1ilhngk98dd', '')&Lampa.Storage.set('jackett_interview','all')&Lampa.Storage.set('parse_in_search', false)&Lampa.Storage.set('parse_lang', 'df');
      if (value == 'jacred_xyz') Lampa.Storage.set('jackett_url', 'https://jacred.xyz')&Lampa.Storage.set('jackett_key', '')&Lampa.Storage.set('jackett_interview', 'all')&Lampa.Storage.set('parse_in_search', false)&Lampa.Storage.set('parse_lang', 'df');
      Lampa.Settings.update();
